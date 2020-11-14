014c3c564b55b6193387328ad85e0987ce426119
zyasus <2997894910@qq.com>
2020-11-14 18:03:52 +0800

    init repo

---
+++ src/main/java/com/yz/community/community/HelloController/HelloController.java
@@ -0,0 +1,17 @@
+package com.yz.community.community.HelloController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello.html";
+    }
+}
